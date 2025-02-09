3d137f4576f249f2fa988433940ca6d672b926d0
GitHub Action <action@github.com>
2025-02-09 19:58:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/cogreanu/languageplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/cogreanu/languageplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.cogreanu.languageplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
